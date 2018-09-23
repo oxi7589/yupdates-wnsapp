38f07d69be286a9fc41ac1dafb43b3c52e0964f4
Oxi <59569071+oxi7589@users.noreply.github.com>
2018-09-23 15:41:11 +0300

    WNS/1.2.2
    command line argument for custom report period instead of 32 days

---
+++ IWnsHandler/ReportRecord.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatsNewShared
+{
+    public class RootRecord
+    {
+        public string Rec = "";
+    }
+
+    public class ReportRecord
+    {
+        public DateTime UpdateFinished;
+        public string ParentUrl = "err";
+        public string ParentPath = "err";
+        public int NumberOfUpdates = -1;
+        public string CustomLine = "";
+        public RootRecord RootRec;
+    }
+}
